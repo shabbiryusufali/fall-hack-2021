493a95d46a9751329fbcbbee17083e4e19661f0c
Dylan Feng <dylanfeng02@gmail.com>
2021-11-27 18:07:29 -0800

    added all the buttons

--- JS and HTML/website.js
+++ JS and HTML/website.js
@@ -3,6 +3,7 @@ document.getElementById("cookie").onclick = cookieClicked;
 var money = 1000;
 
 var upgrades = new Array();
+var money_per_click = 1;
 
 class Upgrade {
     constructor(name, cost, CPS) {
@@ -12,19 +13,32 @@ class Upgrade {
     }
 }
 
-upgrades.push(new Upgrade("upgrd_admitStudent", 10, 0.1));
-upgrades.push(new Upgrade("upgrd_hireInstructor", 1000, 10));
+upgrades.push(new Upgrade("tution", 250, 0)); 
 
-// var upgradeCosts = new Object();
-// upgradeCosts["upgrd_admitStudent"] = 10;
-// upgradeCosts["upgrd_hireInstructor"] = 1000;
-
-// const upgradeCps = new Object();
-// upgradeCps["upgrd_admitStudent"] = 0.1;
-// upgradeCps["upgrd_hireInstructor"] = 10;
+upgrades.push(new Upgrade("upgrd_CMPT105W", 20, 0.1));
+upgrades.push(new Upgrade("upgrd_CMPT295", 900, 5));
+upgrades.push(new Upgrade("upgrd_CMPTCourses", 3850, 10));
+upgrades.push(new Upgrade("upgrd_Student", 6600, 25));
+upgrades.push(new Upgrade("upgrd_Instructor", 15945, 35));
+upgrades.push(new Upgrade("upgrd_LowerCurve", 24000, 60));
+upgrades.push(new Upgrade("upgrd_NewCampus", 44757, 100));
+upgrades.push(new Upgrade("upgrd_DestroyUni", 69420, 150));
+upgrades.push(new Upgrade("upgrd_onlySFU", 69420666, 100000000000000000000000000000000000));
 
 function cookieClicked() {
-    document.getElementById("money").innerHTML = "$" + ++money;
+    money = money + money_per_click;
+    document.getElementById("money").innerHTML = "$" + money;
+}
+
+function upgrade_click(upgrade)
+{
+    if(upgrades[upgrade].cost <= money)
+    {
+        money -= upgrades[upgrade].cost;
+        cookieClicked();
+        money_per_click = money_per_click*2;
+    }
+    
 }
 
 function buyUpgrade(upgrade) {
@@ -41,5 +55,13 @@ function buyUpgrade(upgrade) {
     }
 }
 
-document.getElementById("upgrd_admitStudent").onclick = () => { buyUpgrade(0); }
-document.getElementById("upgrd_hireInstructor").onclick = () => { buyUpgrade(1); }
+document.getElementById("tuition").onclick = () => { upgrade_click(0); }
+document.getElementById("upgrd_CMPT105W").onclick = () => { buyUpgrade(1); }
+document.getElementById("upgrd_CMPT295").onclick = () => { buyUpgrade(2); }
+document.getElementById("upgrd_CMPTCourses").onclick = () => { buyUpgrade(3); }
+document.getElementById("upgrd_Student").onclick = () => { buyUpgrade(4); }
+document.getElementById("upgrd_Instructor").onclick = () => { buyUpgrade(5); }
+document.getElementById("upgrd_LowerCurve").onclick = () => { buyUpgrade(6); }
+document.getElementById("upgrd_NewCampus").onclick = () => { buyUpgrade(7); }
+document.getElementById("upgrd_DestroyUni").onclick = () => { buyUpgrade(8); }
+document.getElementById("upgrd_onlySFU").onclick = () => { buyUpgrade(9); }
