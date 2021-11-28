adb07d4c5cf225262ac3ebc8f762bc142aeaf769
dannyyua <dannyyua@gmail.com>
2021-11-27 17:21:55 -0800

    array of objects

--- JS and HTML/website.js
+++ JS and HTML/website.js
@@ -2,27 +2,41 @@ document.getElementById("cookie").onclick = cookieClicked;
 
 var money = 1000;
 
-var upgradeCosts = new Object();
-upgradeCosts["upgrd_admitStudent"] = 10;
-upgradeCosts["upgrd_hireInstructor"] = 1000;
+var upgrades = new Array();
 
-const upgradeCps = new Object();
-upgradeCps["upgrd_admitStudent"] = 0.1;
-upgradeCps["upgrd_hireInstructor"] = 10;
+class Upgrade {
+    constructor(name, cost, CPS) {
+        this.name = name;
+        this.cost = cost;
+        this.CPS = CPS;
+    }
+}
+
+upgrades += [new Upgrade("upgrd_admitStudent", 10, 0.1)];
+upgrades += [new Upgrade("upgrd_hireInstructor", 1000, 10)];
+
+// var upgradeCosts = new Object();
+// upgradeCosts["upgrd_admitStudent"] = 10;
+// upgradeCosts["upgrd_hireInstructor"] = 1000;
+
+// const upgradeCps = new Object();
+// upgradeCps["upgrd_admitStudent"] = 0.1;
+// upgradeCps["upgrd_hireInstructor"] = 10;
 
 function cookieClicked() {
     document.getElementById("money").innerHTML = "$" + ++money;
 }
 
 function buyUpgrade(upgrade) {
-    console.log("asddas");
-    if (upgradeCosts[upgrade] <= money) {
-        money -= upgradeCosts[upgrade];
-        document.getElementById("money").innerHTML = "$" + ++money;
-        setInterval(() => { money++; document.getElementById("money").innerHTML = "$" + ++money; }, 1/upgradeCps[upgrade] * 1000);
-        upgradeCosts[upgrade] *= 1.15;
+    if (upgrades[upgrade].cost <= money) {
+        money -= upgrades[upgrade].cost;
+        cookieClicked();
+        setTimeout(setInterval(() => { cookieClicked(); }, 1/upgrades[upgrade].CPS * 1000), 1/upgrades[upgrade].CPS * 1000);
+        upgrades[upgrade].cost = Math.round(upgradeCosts[upgrade]*1.15);
+
+        console.log(upgrades[upgrade].cost);
     }
 }
 
-document.getElementById("upgrd_admitStudent").onclick = () => { buyUpgrade("upgrd_admitStudent"); }
-document.getElementById("upgrd_hireInstructor").onclick = () => { buyUpgrade("upgrd_hireInstructor"); }
+document.getElementById("upgrd_admitStudent").onclick = () => { buyUpgrade(0); }
+document.getElementById("upgrd_hireInstructor").onclick = () => { buyUpgrade(1); }
